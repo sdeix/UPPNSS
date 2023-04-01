952bf633b49039ed7e854cc5cbad2acb5d2e033f
sdeix <sdeix2765@gmail.com>
2023-04-01 16:47:07 +0700

    Установка и настройка Composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'sdeix/uppnss',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '324c6ee3884d8f9f8dba973f5acc80cc6565348b',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'sdeix/uppnss' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '324c6ee3884d8f9f8dba973f5acc80cc6565348b',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
