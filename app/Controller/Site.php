632655bef5da4c29f5b04ad1edd40126341f88d5
sdeix <sdeix2765@gmail.com>
2023-04-02 16:03:58 +0700

    Функция регистрации

--- app/Controller/Site.php
+++ app/Controller/Site.php
@@ -5,7 +5,7 @@ namespace Controller;
 use Model\Post;
 use Src\Request;
 use Src\View;
-
+use Model\User;
 
 class Site
 {
@@ -20,4 +20,12 @@ public function index(Request $request): string
    {
        return new View('site.hello', ['message' => 'hello working']);
    }
+   public function signup(Request $request): string
+   {
+       if ($request->method==='POST' && User::create($request->all())){
+           return new View('site.signup', ['message'=>'Вы успешно зарегистрированы']);
+       }
+       return new View('site.signup');
+   }
+
 }
