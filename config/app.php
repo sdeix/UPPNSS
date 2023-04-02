d49fa211f4ad211938e8ef319f72b62bddfa6a10
sdeix <sdeix2765@gmail.com>
2023-04-02 21:24:41 +0700

    Базовая защита от CSRF-атак

--- config/app.php
+++ config/app.php
@@ -9,8 +9,10 @@ return [
        'auth' => \Middlewares\AuthMiddleware::class,
    ],
 'routeAppMiddleware' => [
+   'csrf' => \Middlewares\CSRFMiddleware::class,
    'trim' => \Middlewares\TrimMiddleware::class,
    'specialChars' => \Middlewares\SpecialCharsMiddleware::class,
+
 ],
 
     'validators' => [
